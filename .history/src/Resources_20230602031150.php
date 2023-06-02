59e60705b028375eeec1532519b39533ab1ecf42
SuperNinja0425 <superninja0425@gmail.com>
2023-06-02 03:13:10 -0400

    feat: add get_genetic_map_1000G_GRCh37

--- .history/src/Resources_20230602031150.php
+++ .history/src/Resources_20230602031150.php
@@ -61,6 +61,28 @@ public function _load_genetic_map_HapMapII_GRCh37($filename) {
 
 public function get_genetic_map_1000G_GRCh37(string $pop): array
 {
+    // Get population-specific 1000 Genomes Project genetic map.
+    // From README_omni_recombination_20130507:
+    // Genetic maps generated from the 1000G phased OMNI data.
+    // [Build 37] OMNI haplotypes were obtained from the Phase 1 dataset.
+    // Genetic maps were generated for each population separately using LDhat.
+    // Haplotypes were split into 2000 SNP windows with an overlap of 200 SNPs between each window.
+    // The recombination rate was estimated for each window independently, using a block penalty of 5 for a
+    // total of 22.5 million iterations with a sample being taken from the MCMC chain every 15,000 iterations.
+    // The first 7.5 million iterations were discarded as burn in.
+    // Once rates were estimated, windows were merged by splicing the estimates at the mid-point of the overlapping regions.
+    // LDhat estimates the population genetic recombination rate, rho = 4Ner.
+    // In order to convert to per-generation rates (measured in cM/Mb), the LDhat rates were compared to pedigree-based rates from Kong et al. (2010).
+    // Specifically, rates were binned at the 5Mb scale, and a linear regression performed between the two datasets.
+    // The gradient of this line gives an estimate of 4Ne, allowing the population based rates to be converted to per-generation rates.
+
+    // Returns dict of pandas.DataFrame population-specific 1000 Genomes Project genetic maps if loading was successful, else {}
+    // References:
+    // Adam Auton, May 7th, 2013
+    // The 1000 Genomes Project Consortium., Corresponding authors., Auton, A. et al. A global reference for human genetic variation. Nature 526, 68–74 (2015).
+    // https://doi.org/10.1038/nature15393
+    // https://github.com/joepickrell/1000-genomes-genetic-maps
+
     if ($this->_genetic_map_name !== $pop) {
         $this->_genetic_map = $this->_load_genetic_map_1000G_GRCh37(
             $this->_get_path_genetic_map_1000G_GRCh37($pop)
