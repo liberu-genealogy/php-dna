36d049bfdfdafae3f50fe71b5a2e9df796a5d3b2
Curtis Delicata <curtis.delicata@outlook.com>
2023-07-10 19:14:01 +0000

    Clean up

--- src/Resources.php
+++ src/Resources.php
@@ -5,8 +5,8 @@
  *
  * tools for genetic genealogy and the analysis of consumer DNA test results
  *
- * @author          Laravel Software Ltd <support@laravel-liberu.com>
- * @copyright       Copyright (c) 2020-2023, Laravel Software Ltd
+ * @author          Liberu Software Ltd <support@laravel-liberu.com>
+ * @copyright       Copyright (c) 2020-2023, Liberu Software Ltd
  * @license         MIT
  *
  * @link            http://github.com/laravel-liberu/php-dna
