b2247e7c88707f003be4ffddcb56f9d6a1c1c162
Joshua Kisb <joshuakisb@gmail.com>
2023-09-07 08:36:33 +0300

    add test merge remap false

--- tests/Snps/SnpsMergeTest.php
+++ tests/Snps/SnpsMergeTest.php
@@ -73,7 +73,8 @@ class SnpsMergeTest extends SnpsTest
         $this->assertResults($fromFile, [["merged" => true]]);
     }
 
-    public function testMergeList() {
+    public function testMergeList()
+    {
         $s = new SNPs();
         $results = $s->merge([new SNPs("tests/input/GRCh37.csv"), new SNPs("tests/input/GRCh37.csv")]);
         $this->assertEquals($s->getSnps(), self::snps_GRCh37());
@@ -90,6 +91,92 @@ class SnpsMergeTest extends SnpsTest
                     "rsIndelTest",
                     "rs11928389",
                 ],
+            ],
+        ];
+        $this->assertResults($results, $expectedResults);
+    }
+
+    public function testMergeRemapping()
+    {
+        $s = new SNPs("tests/input/NCBI36.csv");
+
+        $results = $s->merge([new SNPs("tests/input/GRCh37.csv")]);
+
+        // Check that there are no discrepancies in merge positions and genotypes
+        $this->assertCount(0, $s->getDiscrepantMergePositions());
+        $this->assertCount(0, $s->getDiscrepantMergeGenotypes());
+
+        // Compare the 'snps' attribute of 's' with the expected array directly
+        $this->assertEquals($s->getSnps(), self::snps_NCBI36());
+
+        // Check the results of the merge operation
+        $expectedResults = [
+            [
+                "merged" => true,
+                "common_rsids" => [
+                    "rs3094315",
+                    "rs2500347",
+                    "rsIndelTest",
+                    "rs11928389",
+                ],
+            ],
+        ];
+        $this->assertResults($results, $expectedResults);
+    }
+
+    public function testMergeRemapFalse() {
+        $s = new SNPs("tests/input/NCBI36.csv");
+        
+        $results = $s->merge([new SNPs("tests/input/GRCh37.csv")], false);
+    
+        // Check the count of discrepancies in merge positions
+        $this->assertCount(4, $s->getDiscrepantMergePositions());
+        // Compare the discrepancies in merge positions with the expected results
+        $this->assertSame(
+            $s->getDiscrepantMergePositions(),
+            $results[0]["discrepant_position_rsids"]
+        );
+    
+        // Check the count of discrepancies in merge genotypes
+        $this->assertCount(1, $s->getDiscrepantMergeGenotypes());
+        // Compare the discrepancies in merge genotypes with the expected results
+        $this->assertSame(
+            $s->getDiscrepantMergeGenotypes(),
+            $results[0]["discrepant_genotype_rsids"]
+        );
+    
+        // Check the count of discrepancies in both positions and genotypes
+        $this->assertCount(4, $s->getDiscrepantMergePositionsGenotypes());
+        // Compare the discrepancies in both positions and genotypes with the expected results
+        $this->assertSame(
+            $s->getDiscrepantMergePositionsGenotypes(),
+            $results[0]["discrepant_position_rsids"]
+        );
+    
+        // Define the expected array for snps_NCBI36 with the discrepant genotype set to null/NA
+        $expected = self::snps_NCBI36();
+        $expected["rs11928389"]["genotype"] = null;
+    
+        // Compare the 'snps' attribute of 's' with the expected array directly
+        $this->assertEquals($s->getSnps(), $expected);
+    
+        // Check the results of the merge operation
+        $expectedResults = [
+            [
+                "merged" => true,
+                "common_rsids" => [
+                    "rs3094315",
+                    "rs2500347",
+                    "rsIndelTest",
+                    "rs11928389",
+                ],
+                "discrepant_position_rsids" => [
+                    "rs3094315",
+                    "rs2500347",
+                    "rsIndelTest",
+                    "rs11928389",
+                ],
+                "discrepant_genotype_rsids" => ["rs11928389"],
             ],
         ];
         $this->assertResults($results, $expectedResults);
